52c3c6ad1d70ed8549039c53404bb036dfd8817f
spncrhrstn <spencer314@gmail.com>
2021-02-25 05:04:12 +0000

    Update gh-pages to output generated at 0ee38d0

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const searchBtn = document.getElementById('searchBtn');
+const settingsBtn = document.getElementById('settings');
+const weatherBtn = document.getElementById('weather');
 
 searchBtn.addEventListener('click', function(e){
     let query = document.getElementById('searchTerm');
     console.log(query.value);
     e.preventDefault();
-})
+});
+
+settingsBtn.addEventListener('click', function(){
+    alert('this does nothing');
+});
+
+weatherBtn.addEventListener('click', function(){
+    alert('this does nothing');
+});
