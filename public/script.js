29ab07ea3be75f09dd06e64757c8804b3d425640
Spencer Harston <spencer314@gmail.com>
2021-02-26 23:30:30 -0700

    Update greeting to change based on time of day

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const searchBtn = document.getElementById('searchBtn');
 const settingsBtn = document.getElementById('settings');
 const weatherBtn = document.getElementById('weather');
+const greeting = document.getElementById('greeting');
 
 searchBtn.addEventListener('click', function(e){
     let query = document.getElementById('searchTerm');
@@ -15,3 +16,23 @@ settingsBtn.addEventListener('click', function(){
 weatherBtn.addEventListener('click', function(){
     alert('this does nothing');
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+    updateGreeting();
+});
+
+function updateGreeting(){
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours();
+    if(currentHour >=0 && currentHour < 6){
+        greeting.textContent = "good early morning";
+    } else if (currentHour >= 6 && currentHour < 12){
+        greeting.textContent = "good morning";
+    } else if (currentHour >= 12 && currentHour < 18){
+        greeting.textContent = "good afternoon";
+    } else{
+        greeting.textContent = "good evening";
+    }
+
+    greeting.textContent += " sir/madam";
+}
