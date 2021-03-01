f9aec94abb86f33e781268a5818fbe1bad429711
spncrhrstn <spencer314@gmail.com>
2021-03-01 05:09:37 +0000

    Update gh-pages to output generated at 9695b1a

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+/* Elements */
 const searchBtn = document.getElementById('searchBtn');
 const settingsBtn = document.getElementById('settings');
 const weatherBtn = document.getElementById('weather');
 const greeting = document.getElementById('greeting');
 
+/* Functions */
+function getGreeting(){
+    let currentHour = new Date().getHours();
+    let msg = "";
+    if(currentHour >=0 && currentHour < 6){
+        msg = "good early morning";
+    } else if (currentHour >= 6 && currentHour < 12){
+        msg = "good morning";
+    } else if (currentHour >= 12 && currentHour < 18){
+        msg = "good afternoon";
+    } else{
+        msg = "good evening";
+    }
+
+    msg += " sir/madam";
+    return msg;
+}
+
+/* Events */
 searchBtn.addEventListener('click', function(e){
+    e.preventDefault();
     let query = document.getElementById('searchTerm');
     console.log(query.value);
-    e.preventDefault();
 });
 
 settingsBtn.addEventListener('click', function(){
@@ -17,22 +37,9 @@ weatherBtn.addEventListener('click', function(){
     alert('this does nothing');
 });
 
+// run when document ready
 document.addEventListener('DOMContentLoaded', function(){
-    updateGreeting();
+    
+    // update greeting
+    greeting.textContent = getGreeting();
 });
-
-function updateGreeting(){
-    let currentTime = new Date();
-    let currentHour = currentTime.getHours();
-    if(currentHour >=0 && currentHour < 6){
-        greeting.textContent = "good early morning";
-    } else if (currentHour >= 6 && currentHour < 12){
-        greeting.textContent = "good morning";
-    } else if (currentHour >= 12 && currentHour < 18){
-        greeting.textContent = "good afternoon";
-    } else{
-        greeting.textContent = "good evening";
-    }
-
-    greeting.textContent += " sir/madam";
-}
